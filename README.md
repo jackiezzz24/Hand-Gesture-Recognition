9ca643f6f977a640eb52ecedef6d3f7992edf024
jackiezzz24 <jiaqi.zhao24@gmail.com>
2024-04-01 20:48:49 -0700

    setup

---
+++ README.md
@@ -0,0 +1,21 @@
+# Hand Gesture Recognition Application 
+
+### Authors: Jiaqi Zhao, Kexuan Chen, Zhimin Liang
+
+## Development Environment
+
+Operating system: Windows
+
+IDE: VS Code 
+
+## How to run the program
+
+Run the main.py 
+
++ In the terminal, type `python main.py`
+
+
+
+## Links 
+
+
---
+++ main.py
@@ -0,0 +1,19 @@
+# Name: Jiaqi Zhao, Kexuan Chen, Zhimin Liang
+# Date: 
+# 
+
+# import statements
+import sys
+import torch
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
+import cv2
+
+# main function 
+def main(argv):
+
+    return
+
+if __name__ == "__main__":
+    main(sys.argv)
