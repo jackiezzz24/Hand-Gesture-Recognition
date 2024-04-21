85c5b49e44ab49c4e8851fc99daa70ffbd67a72a
Livia328 <374606755@qq.com>
2024-04-21 14:19:10 -0700

    implement gesture left and gesture right

--- main.py
+++ main.py
@@ -3,13 +3,18 @@
 # 
 
 # import statements
+import os
 import sys
 import cv2
+from cvzone.HandTrackingModule import HandDetector
 
 # main function
 def main(argv):
     # video variables
     width, height = 1280, 720
+    width2, height2 = int(128 * 2), int(72 * 2)
+    pptPath = "presentation"
+    threshold = 300 # line to trigger gesture detection
 
     # camera setup
 
@@ -18,9 +23,70 @@ def main(argv):
     cap.set(3,width)
     cap.set(4,height)
 
+    # get ppt image
+    pptImages = sorted(os.listdir(pptPath), key=lambda x: int(x.split('.')[0])) # sorted the images to make sure 10.jpg would not occur after 1.jpg
+    print(pptImages)
+
+    # index of image showing
+    imgNumber = 0
+    buttonPressed = False
+    buttonCounter = 0
+    #10 frame after the previous gesture, we do not accept new gestures
+    #otherwise it will apply the gestures too quickly
+    duration = 30 
+
+    # get hand detector
+    detector = HandDetector(detectionCon=0.8, maxHands=1)
+
     while True:
         success, frame = cap.read()
+        #frame = cv2.flip(frame, 1)
+        # get the image path
+        currentImgPath = os.path.join(pptPath, pptImages[imgNumber])
+        currentImg = cv2.imread(currentImgPath)
+
+        hands, frame = detector.findHands(frame)
+
+        cv2.line(frame, (0, threshold), (width, threshold), (0, 255, 0), 10)
+
+        if hands and buttonPressed is False:
+            hand = hands[0]
+            # check how many fingers are up
+            fingers = detector.fingersUp(hand)
+            centerX, centerY = hand['center']
+            # print(fingers)
+
+            # only check the hand when it is above the threshold
+            if centerY <= threshold:
+                # 1. left, show previous slide
+                if fingers == [1, 0, 0, 0, 0]:
+                    # print("left")
+                    if imgNumber > 0:
+                        buttonPressed = True
+                        imgNumber -= 1
+
+                # 2. right, show next slide
+                if (fingers == [0, 0, 0, 0, 1]):
+                    # print("right")
+                    if imgNumber < len(pptImages) - 1:
+                        buttonPressed = True
+                        imgNumber += 1
+
+        # change the statues of buttonPressed
+        if buttonPressed:
+            buttonCounter += 1
+            if buttonCounter > duration:
+                buttonCounter = 0
+                buttonPressed = False
+
+
+        # resize and rearrange the windows
+        frameSmall = cv2.resize(frame, (width2, height2))
+        h,w,_ = currentImg.shape
+        currentImg[0:height2, w-width2:w] = frameSmall # put the video on the right corner of ppt
+
         cv2.imshow("frame", frame)
+        cv2.imshow("ppt", currentImg)
         key = cv2.waitKey(1)
 
         if key == ord('q'):
